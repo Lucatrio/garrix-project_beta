14aa2ee34428ae180631359f20a6236b11388859
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-23 07:56:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const commands = require('./commands.js');
 
 client.on("ready", async () => {
   console.log(`${client.user.tag} logged in ${client.guilds.size} guilds`)
