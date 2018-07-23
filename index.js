eb4983da1414ae115841e46c12872c52e148782a
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-23 17:59:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const commands = require('./commands.js');
 
 client.on("ready", async () => {
   console.log(`${client.user.tag} logged in ${client.guilds.size} guilds`)
-  client.user.setActivity('the queue', { type: 'WATCHING' })
+  client.user.setActivity(url: 'https://www.twitch.tv/monstercat', { type: 'STREAMING' })
 }); 
 client.login(process.env.TOKEN);
