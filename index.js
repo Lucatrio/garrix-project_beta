35176f324923b89e6532af87bd7857bbcedb9522
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2018-07-23 20:30:17 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const commands = require('./commands.js');
-
-client.on("ready", async () => {
-  console.log(`${client.user.tag} logged in ${client.guilds.size} guilds`)
-  client.user.setActivity('the queue', { type: 'WATCHING' })
-}); 
-client.login(process.env.TOKEN);
