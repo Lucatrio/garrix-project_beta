235371dc7f1664a8ca83a97e9186455b734cc9e6
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-24 19:22:09 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,16 +3,6 @@ const moment = require("moment");
 const momentduration = require("moment-duration-format");
 
 module.exports.run = async (client, message, args) => {
- let msg = 'Uptime: ';
- const totalSeconds = process.uptime();
- const days = Math.floor((totalSeconds % 31536000) / 86400);
- const hours = _.parseInt(totalSeconds / 3600) % 24; 
- const minutes = _.parseInt(totalSeconds / 60) % 60;
- const seconds = Math.floor(totalSeconds % 60);
- msg += days >= 1 ? `${days}d ` : '';
- msg += hours < 10 ? `0${hours}:` : `${hours}:`;
- msg += minutes < 10 ? `0${minutes}:` : `${minutes}:`;
- msg += seconds < 10 ? `0${seconds}` : `${seconds}`;
   
     let embed = new Discord.RichEmbed()
     .setColor("RED")
@@ -20,7 +10,6 @@ module.exports.run = async (client, message, args) => {
     .addField(`Guilds `, client.guilds.size)
     .addField(`Users `, client.users.size)
     .addField(`Channels `, client.channels.size)
-    .addField(`Uptime `, msg);
 
 message.channel.send(embed);
 }
