32b499f1cdfe9a5c40b034d3e7ef64252d264e83
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-24 14:47:29 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js")
+const moment = require("moment");
+const momentduration = require("moment-duration-format");
+
 module.exports.run = async (client, message, args) => {
     let embed = new Discord.RichEmbed()
+	let totalSeconds = (client.uptime / 1000);
+	let hours = Math.floor(totalSeconds / 3600);
+	totalSeconds %= 3600;
+	let minutes = Math.floor(totalSeconds / 60);
+	let seconds = totalSeconds % 60;
+	
+	let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
     .setColor("RED")
     .setTitle('> General <')
     .addField(`Guilds `, client.guilds.size)
@@ -9,4 +19,8 @@ module.exports.run = async (client, message, args) => {
     .addField(`Uptime `, uptime)
 
 message.channel.send(embed);
+}
+
+module.exports.run = {
+    name: "stats"
 }
