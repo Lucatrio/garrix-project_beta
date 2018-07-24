bed6de9477cf21e00e0b8363f541bfbd57f9a607
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-23 20:41:18 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js")
+const Discord = require("discord.js")
 module.exports.run = async (client, message, args) => {
     let embed = new Discord.RichEmbed()
     .setColor("RED")
