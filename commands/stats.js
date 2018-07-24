af89f5feea667c7607cd7bb0dcc8f8fcd2e8c58a
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-24 15:12:09 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,25 +3,24 @@ const moment = require("moment");
 const momentduration = require("moment-duration-format");
 
 module.exports.run = async (client, message, args) => {
-function uptime() {
-  let msg = 'Uptime: ';
-  const totalSeconds = process.uptime();
-  const days = Math.floor((totalSeconds % 31536000) / 86400);
-  const hours = _.parseInt(totalSeconds / 3600) % 24;
-  const minutes = _.parseInt(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds % 60);
-  msg += days >= 1 ? `${days}d ` : '';
-  msg += hours < 10 ? `0${hours}:` : `${hours}:`;
-  msg += minutes < 10 ? `0${minutes}:` : `${minutes}:`;
-  msg += seconds < 10 ? `0${seconds}` : `${seconds}`;
-}
+ let msg = 'Uptime: ';
+ const totalSeconds = process.uptime();
+ const days = Math.floor((totalSeconds % 31536000) / 86400);
+ const hours = _.parseInt(totalSeconds / 3600) % 24; 
+ const minutes = _.parseInt(totalSeconds / 60) % 60;
+ const seconds = Math.floor(totalSeconds % 60);
+ msg += days >= 1 ? `${days}d ` : '';
+ msg += hours < 10 ? `0${hours}:` : `${hours}:`;
+ msg += minutes < 10 ? `0${minutes}:` : `${minutes}:`;
+ msg += seconds < 10 ? `0${seconds}` : `${seconds}`;
+  
     let embed = new Discord.RichEmbed()
     .setColor("RED")
     .setTitle('> General <')
     .addField(`Guilds `, client.guilds.size)
     .addField(`Users `, client.users.size)
     .addField(`Channels `, client.channels.size)
-    .addField(`Uptime `, uptime.msg);
+    .addField(`Uptime `, msg);
 
 message.channel.send(embed);
 }
