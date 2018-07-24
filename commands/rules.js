13bd884b20a9207a7d39cb610e0c0c269a24c7dd
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2018-07-23 20:27:55 -0400

    Add files via upload

---
+++ commands/rules.js
@@ -0,0 +1,5 @@
+const discord = require("discord.js")
+module.exports.run = async (client, message, args) => {
+    message.channel.send('Please follow these rules if you don\'t you will be punished \n\n1. No racisum nor sexism will be tolerated in this guild \n2. If you want your discord bot in this guild make sure you apply and meet the requirement\'s \n3. If you bot is found posting nsfw content outside of the nsfw channel you and your bot will be punished \n4. Please do not advertise your discord server nor your media platforms if you do so you will be removed \n5. No extra permissions for be accounted for bots please do not ask!');
+
+}
---
+++ commands/support.js
@@ -0,0 +1,8 @@
+const discord = require("discord.js")
+module.exports.run = async (client, message, args) => {
+    let discordlink = '[here](https://discord.gg/XWMf7P)'
+    let embed = new Discord.RichEmbed()
+        .setColor('RED')
+        .setTitle(`Here is the support server ${discord}`)
+    message.channel.send(embed);
+}
